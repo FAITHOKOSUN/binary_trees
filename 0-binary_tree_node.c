465bd32e2604e7824d2a1534e199131b4b16b538
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-12-13 07:56:11 -0800

    passed betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,15 +2,16 @@
 
 /**
  * binary_tree_node - Create a binary tree node
- * @parent - pointer to the parent node of the node to create
- * @value - the value to put in the new node
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value to put in the new node
  *
- * Return - error on failure and new node on success
+ * Return: error on failure and new node on success
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new =  malloc(sizeof(binary_tree_t));
+
 	if (new == NULL)
 		return (NULL);
 
