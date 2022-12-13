5f8a560ae93a90bc7fd0ea594ce76795aaca467e
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-12-13 08:16:16 -0800

    add a node to the left of a parent

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/** binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: a pointer to the node to insert the left-child in
+ * @value: the value to store in the new node
+ * Return: error on fsilure and new node on success
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+	
+	if (parent == NULL)
+		return (NULL);
+	
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+
+	if (new->n < parent->n)
+	{
+		new->parent = parent;
+		parent->left = new;
+	}
+	return (new);
+}
